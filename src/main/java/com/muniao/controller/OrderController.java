bf88b423c7d7f3b87c467075e42fcee0b27a70fa
scot <scotmouton@gmail.com>
2018-06-15 17:51:40 +0800

    更新订单显示方式   查询方式  以及取消确认问题

--- src/main/java/com/muniao/controller/OrderController.java
+++ src/main/java/com/muniao/controller/OrderController.java
@@ -3,8 +3,10 @@ package com.muniao.controller;
 import com.muniao.entity.OrderDetail;
 import com.muniao.service.OrderDetailService;
 import com.muniao.service.OrderService;
+import com.sun.org.apache.xpath.internal.SourceTree;
 import jdk.net.SocketFlow;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.executor.ReuseExecutor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -13,6 +15,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -49,12 +55,12 @@ public class OrderController {
      * @Author: Scot
      * @Date: 2018/6/12 15:22
      */
-    @RequestMapping(value = "orderdetail/{orderId}")
-    public String selectOrderDetail(@PathVariable("orderId")int id , Model model){
-        OrderDetail orderDetail=orderDetailService.selectOrderDetail(id);
-        model.addAttribute("orderDetail",orderDetail);
-        return "/orderdetail";
-    }
+//    @RequestMapping(value = "orderdetail/{orderId}")
+//    public String selectOrderDetail(@PathVariable("orderId")int id , Model model){
+//        OrderDetail orderDetail=orderDetailService.selectOrderDetail(id);
+//        model.addAttribute("orderDetail",orderDetail);
+//        return "/orderdetail";
+//    }
 
     /**
      * 订单状态:
@@ -66,6 +72,7 @@ public class OrderController {
      * 6 等待确认
      * 7 已取消订单
      * 8 完成
+     * 9
      * */
     @RequestMapping(value = "landlordcommit/{orderId}/{orderStatus}/{buyerId}")
     public String commitOrder(@PathVariable("orderId")int orderId,
@@ -97,10 +104,89 @@ public class OrderController {
         return "/lodgerorder";
     }
 
+    /**
+     * 查找当前用户下所有订单
+     * @param id
+     * @param model
+     * @return
+     */
     @RequestMapping(value = "lodgerorder/{currentUserId}")
     public String selectAllOrders(@PathVariable("currentUserId")int id ,Model model){
         List <com.muniao.entity.Order> orders=orderService.selectAllOrders(id);
         model.addAttribute("orders",orders);
+
+        return "/lodgerorder";
+    }
+
+    /**
+     * 查找当前用户下某个订单详情
+     * @param id
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "orderdetail/{orderId}")
+    public String selectOrderDetail(@PathVariable("orderId")int id , Model model){
+        com.muniao.entity.Order order=orderService.selectOneOrder(id);
+        model.addAttribute("order",order);
+        return "/orderDetail";
+    }
+
+    @RequestMapping(value = "refund/{orderId}")
+    public String refundCal(@PathVariable("orderId") int  orderId,Model model){
+        Calendar cal=Calendar.getInstance();
+        com.muniao.entity.Order order=orderService.selectOneOrder(orderId);
+        Date date2=order.getOrderDetail().getCheckInTime();
+        Date date3=order.getOrderDetail().getDepartureTime();
+        SimpleDateFormat sdf=new SimpleDateFormat("yy-MM-dd");
+        try {
+            Date formatDate1=sdf.parse(new Date(System.currentTimeMillis()).toString());
+            Date formatDate2=sdf.parse(date2.toString());
+            Date formatDate3=sdf.parse(date3.toString());
+            int result=formatDate1.compareTo(formatDate2);
+            int result1=formatDate1.compareTo(formatDate3);
+            Double refundMoney = Double.valueOf(0);
+
+            if(formatDate2.before(formatDate1)&&formatDate3.after(formatDate1)){
+                    System.out.println("入住后退款");
+                    int days1 = (int )(formatDate1.getTime() - formatDate2.getTime()) / (1000 * 24 * 60 * 60);
+                    Double actullyPay=order.getOrderDetail().getRoom().getPrice()*days1;
+                     refundMoney = order.getOrderDetail().getTotalPrice()-actullyPay;
+                    System.out.println("应退金额:"+refundMoney);
+                    orderService.changeOrderStatus("5", orderId);
+            }else if (formatDate2.after(formatDate1)){
+                switch (order.getOrderStatus()){
+                    case "4-6":
+                        System.out.println("无须退款");
+                        orderService.changeOrderStatus("7",orderId);
+                        break;
+                    case "4":
+                        System.out.println("无须退款");
+                        orderService.changeOrderStatus("7",orderId);
+                        break;
+                    case "2-6":
+                        refundMoney=order.getOrderDetail().getTotalPrice();
+                        System.out.println("应退金额"+refundMoney);
+                        orderService.changeOrderStatus("5",orderId);
+                        break;
+                    case "2":
+                        refundMoney=order.getOrderDetail().getTotalPrice();
+                        System.out.println("应退金额"+refundMoney);
+                        orderService.changeOrderStatus("5",orderId);
+                        break;
+                    case "3":
+                        refundMoney=order.getOrderDetail().getTotalPrice();
+                        System.out.println("应退金额"+refundMoney);
+                        orderService.changeOrderStatus("5",orderId);
+                        break;
+                }
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+
+
 
         return "/lodgerorder";
     }
